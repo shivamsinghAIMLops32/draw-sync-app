cb64b12c6381fe5c624c1ecabdc4c0fad62fd852
Shivam Singh <136737397+shivamsinghAIMLops32@users.noreply.github.com>
2025-11-07 18:54:30 +0530

    database package done with prisma

---
+++ packages/database/src/client.ts
@@ -0,0 +1,10 @@
+import { PrismaClient } from "../generated/prisma/client";
+import dotenv from "dotenv";
+dotenv.config();
+
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+
+export const prisma =
+  globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
